733713e5896b1da023c07f0a7f352dc77a770a1a
loctodalee <91656805+loctodalee@users.noreply.github.com>
2023-09-19 23:16:54 +0700

    header-done

--- src/component/Layout/components/Header/components.js
+++ src/component/Layout/components/Header/components.js
@@ -17,36 +17,36 @@ export const components = [
             },
         ],
     },
-    // {
-    //     name: 'Know the zoo',
-    //     path: '/knowthezoo',
-    //     sub: [
-    //         {
-    //             name: 'know the zoo1',
-    //             path: '/knowthezoo1',
-    //         },
-    //         {
-    //             name: 'know the zoo2',
-    //             path: '/knowthezoo2',
-    //         },
-    //     ],
-    // },
-    // {
-    //     name: 'Gallery',
-    //     path: '/Gallery',
-    // },
-    // {
-    //     name: 'blog',
-    //     path: '/blog',
-    //     sub: [
-    //         {
-    //             name: 'blog1',
-    //             path: '/blog1',
-    //         },
-    //         {
-    //             name: 'blog2',
-    //             path: '/blog2',
-    //         },
-    //     ],
-    // },
+    {
+        name: 'Know the zoo',
+        path: '/knowthezoo',
+        sub: [
+            {
+                name: 'Know the zoo1',
+                path: '/knowthezoo1',
+            },
+            {
+                name: 'Know the zoo2',
+                path: '/knowthezoo2',
+            },
+        ],
+    },
+    {
+        name: 'Gallery',
+        path: '/Gallery',
+    },
+    {
+        name: 'Blog',
+        path: '/blog',
+        sub: [
+            {
+                name: 'Blog1',
+                path: '/blog1',
+            },
+            {
+                name: 'Blog2',
+                path: '/blog2',
+            },
+        ],
+    },
 ];
