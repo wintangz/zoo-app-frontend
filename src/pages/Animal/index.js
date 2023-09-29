95a7ef469f40a37b2b1d2ac6d819690f1934ad4c
loctodalee <91656805+loctodalee@users.noreply.github.com>
2023-09-30 00:01:51 +0700

    create route animal

---
+++ src/pages/Animal/index.js
@@ -0,0 +1,5 @@
+function Animal() {
+    return <h2>Animal page</h2>;
+}
+
+export default Animal;
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,12 @@ import aboutBanner from '~/assets/img/t1.jpg';
 import Home from '~/pages/Home';
 import About from '~/pages/About';
 import News from '~/pages/News';
+import Animal from '~/pages/Animal';
 
 const publicRoutes = [
     { path: '/', component: Home, name: 'Home' },
     { path: 'about', component: About, layout: NormalLayout, name: 'About', bannerImage: aboutBanner },
-    { path: 'news', component: News, layout: NormalLayout, name: 'News' },
+    { path: 'animal', component: Animal, layout: NormalLayout, name: 'Animal', babelImage: aboutBanner },
 ];
 
 // private routes dont login will redirect to login pages
