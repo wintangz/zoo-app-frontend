74be2d561c99fd057a638ea7d05f9cfe080e64a1
loctodalee <91656805+loctodalee@users.noreply.github.com>
2023-09-29 23:25:13 +0700

    banner

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,43 @@
-import homepage from '~/assets/img/homepage.jpg';
-
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
 function Home() {
+    const cx = classNames.bind(styles);
     return (
         <>
             {/* <DefaultLayout /> */}
             <div class="home--container">
-                <div class="col-md-6">
-                    <h2 color="red">Welcome To Wilddale</h2>
+                <div className={cx('information')}>
+                    <div className={cx('welcome')}>
+                        <p>Welcome To SaiGon Zoo</p>
+                    </div>
+                    <div className={cx('information--title')}>
+                        <h1>
+                            Words About Best Animal Worlds <span>Big Zoo.</span>
+                        </h1>
+                    </div>
+                    <div className={cx('information--decs')}>
+                        <p>
+                            The Wildlife conservation is long term commitment and journey that requires the cooperation
+                            of everyone in the community. We are very much evolved into practice of creating better
+                            place.
+                        </p>
+                    </div>
+                    <div className={cx('information--table')}>
+                        <div className={cx('information--table--front')}>
+                            <div className={cx('table--front')}>
+                                <h2 className={cx('title')}></h2>
+                                <p className={cx('content')}></p>
+                            </div>
+                            <div className={cx('table--front')}>
+                                <h2 className={cx('title')}></h2>
+                                <p className={cx('content')}></p>
+                            </div>
+                        </div>
+                        <div className={cx('information--table--down')}>
+                            <div className={cx('table--down')}></div>
+                            <div className={cx('table--down')}></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
