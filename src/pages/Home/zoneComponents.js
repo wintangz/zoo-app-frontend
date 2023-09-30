ea97d1524403a14900302582708e6c9738fffdfb
loctodalee <91656805+loctodalee@users.noreply.github.com>
2023-09-30 13:09:11 +0700

    advance banner

---
+++ src/pages/Home/zoneComponents.js
@@ -0,0 +1,10 @@
+import zone1 from '~/assets/img/zone1.jpg';
+
+const zones = [
+    {
+        id: 1,
+        name: 'Tropical Rainforest',
+        imgUrl: zone1,
+        hoverImage: '',
+    },
+];
