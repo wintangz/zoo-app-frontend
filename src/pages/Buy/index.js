8371748d1c01cc23e1fd4c3f9d76c502565ac6d0
conboaduide <anhvnvse172006@fpt.edu.vn>
2023-09-30 14:10:16 +0700

    Add Ticket

--- src/pages/Buy/index.js
+++
@@ -1,5 +0,0 @@
-function Buy() {
-    return <h2>Buy page</h2>;
-}
-
-export default Buy;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,17 @@ import { NormalLayout } from '~/component/Layout';
 import aboutBanner from '~/assets/img/t1.jpg';
 
 import Home from '~/pages/Home';
-import About from '~/pages/About';
 import News from '~/pages/News';
 import Animals from '~/pages/Animals';
+import Ticket from '~/pages/Ticket';
+import About from '~/pages/About';
 
 const publicRoutes = [
     { path: '/', component: Home, name: 'Home' },
-    { path: 'about', component: About, layout: NormalLayout, name: 'About' },
     { path: 'news', component: News, layout: NormalLayout, name: 'News' },
     { path: 'animals', component: Animals, layout: NormalLayout, name: 'Animals' },
+    { path: 'ticket', component: Ticket, layout: NormalLayout, name: 'Ticket' },
+    { path: 'about', component: About, layout: NormalLayout, name: 'About' },
 ];
 
 // private routes dont login will redirect to login pages
