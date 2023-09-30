ec181e227248c2d7c69ada17d36507807d709c9d
conboaduide <anhvnvse172006@fpt.edu.vn>
2023-10-01 02:51:43 +0700

    Edit Ticket

--- src/pages/Ticket/index.js
+++ src/pages/Ticket/index.js
@@ -7,10 +7,34 @@ import styles from './Ticket.module.scss';
 function Ticket() {
     return (
         <>
-            <NormalBanner>
-                <img src={logo} />
-            </NormalBanner>
-            <h1>ticket</h1>
+            <NormalBanner />
+            <div className={styles.container}>
+                <div className={styles.void}>
+                    <div className={styles.col}>
+                        <div className={styles.header}>
+                            <h2 className={styles.headline}>Tickets</h2>
+                        </div>
+                    </div>
+                    <div className={styles.col}>
+                        <div className={styles.slider_waraper}>
+                            <div className={styles.swiper_warpper}>
+                                <div className={styles.card}>
+                                    <a href="" title="Book now" className={styles.link_cover}>
+                                        Book now
+                                    </a>
+                                    <h1 className={styles.content}>
+                                        book now book now book now book now book now book now book now book now book now
+                                        book now book now book now book now book now book now book now book now book now
+                                        book now book now book now book now book now book now book now book now book now
+                                        book nowbook now book now book now book now book now book now book now book now
+                                        book now
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
