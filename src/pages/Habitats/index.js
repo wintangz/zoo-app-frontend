9d7ab5e37725d9178270e5b8f18ca4e2acefc372
Hoàng Thành <hoangthanh5264@gmail.com>
2023-09-30 19:15:54 +0700

    habitat background

--- src/pages/Habitats/index.js
+++ src/pages/Habitats/index.js
@@ -1,5 +1,33 @@
+import React from 'react';
+import styles from './Habitats.module.scss';
+import { useState } from 'react';
+
+import bg from '~/assets/img/news-background.png';
+
 function Habitats() {
-    return <h2>Habitats page</h2>;
+    return (
+        <>
+            <div className={styles.habitat__container}>
+                <div className={styles.bg__container}>
+                    <div className={styles.bg__img}>
+                        <img src={bg}></img>
+                    </div>
+                    <div className={styles.content}>
+                        <h1 className={styles.headingtile}>
+                            <div>Habitats</div>
+                        </h1>
+                    </div>
+                </div>
+                <div className={styles.main__content}>
+                    <div className={styles.dropdown__container}>
+                        <div className={styles.dropdown__bar}>
+                            <h1>Bar here</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Habitats;
