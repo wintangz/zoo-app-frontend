43e224ee58c5cb1b2341ce7fc89b5aa2b6583998
loctodalee <91656805+loctodalee@users.noreply.github.com>
2023-09-30 10:37:35 +0700

    done home information

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,62 +1,78 @@
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
+import { Link } from 'react-router-dom';
 function Home() {
     const cx = classNames.bind(styles);
     return (
         <>
             {/* <DefaultLayout /> */}
             <div className={cx('home--container')}>
-                <div className={cx('information')}>
-                    <div className={cx('information--left')}>
-                        <div className={cx('welcome')}>
-                            <p>Welcome To SaiGon Zoo</p>
-                        </div>
-                        <div className={cx('information--title')}>
-                            <h1>
-                                Words About Best Animal Worlds <span>Big Zoo.</span>
-                            </h1>
-                        </div>
-                        <div className={cx('information--decs')}>
-                            <p>
-                                The Wildlife conservation is long term commitment and journey that requires the
-                                cooperation of everyone in the community. We are very much evolved into practice of
-                                creating better place.
-                            </p>
-                        </div>
-                        <div className={cx('information--table')}>
-                            <div className={cx('information--table--front')}>
-                                <div className={cx('table--front')}>
-                                    <h2 className={cx('title')}>Wildlife Conservation</h2>
-                                    <p className={cx('content')}>
-                                        Saving nature is at the very heart of what we do as WildDale.
-                                    </p>
+                <div className="infomation--content">
+                    <div className={cx('information')}>
+                        <div className={cx('information--left')}>
+                            <div className={cx('welcome')}>
+                                <p>Welcome To SaiGon Zoo</p>
+                            </div>
+                            <div className={cx('information--title')}>
+                                <h1>
+                                    Words About Best Animal Worlds <span>Big Zoo.</span>
+                                </h1>
+                            </div>
+                            <div className={cx('information--decs')}>
+                                <p>
+                                    The Wildlife conservation is long term commitment and journey that requires the
+                                    cooperation of everyone in the community. We are very much evolved into practice of
+                                    creating better place.
+                                </p>
+                            </div>
+                            <div className={cx('information--table')}>
+                                <div className={cx('information--table--front')}>
+                                    <div className={cx('table--front')}>
+                                        <h2 className={cx('title')}>Wildlife Conservation</h2>
+                                        <p className={cx('content')}>
+                                            Saving nature is at the very heart
+                                            <br />
+                                            of what we do as WildDale.
+                                        </p>
+                                    </div>
+                                    <div className={cx('table--front')}>
+                                        <h2 className={cx('title')}>Save Wildlife Habitat</h2>
+                                        <p className={cx('content')}>
+                                            We want to make life better place
+                                            <br />
+                                            for animals living in the wild
+                                        </p>
+                                    </div>
                                 </div>
-                                <div className={cx('table--front')}>
-                                    <h2 className={cx('title')}>Save Wildlife Habitat</h2>
-                                    <p className={cx('content')}>
-                                        We want to make life better place for animals living in the wild
-                                    </p>
+                                <div className={cx('information--table--down')}>
+                                    <div className={cx('table--down')}>
+                                        <h2 className={cx('title')}>Interruption of Nature</h2>
+                                        <p className={cx('content')}>
+                                            Investigates nature interruption in
+                                            <br />
+                                            relevance ways of practice!
+                                        </p>
+                                    </div>
+                                    <div className={cx('table--down')}>
+                                        <h2 className={cx('title')}>A Safari Volunteering</h2>
+                                        <p className={cx('content')}>
+                                            A friendly way to travel and share
+                                            <br />
+                                            spaces with majestic animals
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={cx('information--table--down')}>
-                                <div className={cx('table--down')}>
-                                    <h2 className={cx('title')}>Interruption of Nature</h2>
-                                    <p className={cx('content')}>
-                                        Investigates nature interruption in relevance ways of practice!
-                                    </p>
-                                </div>
-                                <div className={cx('table--down')}>
-                                    <h2 className={cx('title')}>A Safari Volunteering</h2>
-                                    <p className={cx('content')}>
-                                        A Safari Volunteering A friendly way to travel and share spaces with majestic
-                                        animals
-                                    </p>
-                                </div>
+                            <div className={cx('information--buttons')}>
+                                <Link to="/about">More About Us!</Link>
+                                <Link to="/contact">Contact Us!</Link>
                             </div>
                         </div>
-                    </div>
-                    <div className={cx('information--right')}>
-                        <div className={cx('information--imgae')}></div>
+                        <div className={cx('information--right')}>
+                            <div className={cx('information--img')}>
+                                <div className={cx('information--img--background')}></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
