59d0fda9082ba6f61bb9879f2470d98f42bb555e
conboaduide <anhvnvse172006@fpt.edu.vn>
2023-10-01 18:56:41 +0700

    Ticket 2.0

--- src/pages/Ticket/index.js
+++ src/pages/Ticket/index.js
@@ -11,6 +11,18 @@ function Ticket() {
             <div className={styles.container}>
                 <div className={styles.void}>
                     <div className={styles.col}>
+                        <header className={styles.ce_header}>
+                            <h1 className={styles.ce_headline}>Welcome to SaiGon Zoo!!</h1>
+                        </header>
+                        <div className={styles.rte}>
+                            <p className={styles.text_center}>
+                                Zoo Berlin is in a class of its own! Not only is it the oldest and most frequently
+                                visited zoo in the country, it is also home to the largest variety of species of any zoo
+                                in the world
+                            </p>
+                        </div>
+                    </div>
+                    <div className={styles.col}>
                         <div className={styles.header}>
                             <h2 className={styles.headline}>Tickets</h2>
                         </div>
@@ -19,16 +31,33 @@ function Ticket() {
                         <div className={styles.slider_waraper}>
                             <div className={styles.swiper_warpper}>
                                 <div className={styles.card}>
-                                    <a href="" title="Book now" className={styles.link_cover}>
+                                    <a
+                                        href="http://localhost:3000/ticket"
+                                        title="Book now"
+                                        className={styles.link_cover}
+                                    >
                                         Book now
                                     </a>
-                                    <h1 className={styles.content}>
-                                        book now book now book now book now book now book now book now book now book now
-                                        book now book now book now book now book now book now book now book now book now
-                                        book now book now book now book now book now book now book now book now book now
-                                        book nowbook now book now book now book now book now book now book now book now
-                                        book now
-                                    </h1>
+                                    <div className={styles.content}>
+                                        <div className={styles.text_wrapper}>
+                                            <h1 className={styles.text}>Adult Tickets</h1>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className={styles.card}>
+                                    <a
+                                        href="http://localhost:3000/ticket"
+                                        title="Book now"
+                                        className={styles.link_cover}
+                                    >
+                                        Book now
+                                    </a>
+                                    <div className={styles.content}>
+                                        <div className={styles.text_wrapper}>
+                                            <h1 className={styles.text}>Children Tickets</h1>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
