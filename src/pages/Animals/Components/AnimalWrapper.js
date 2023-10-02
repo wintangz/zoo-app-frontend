26f21f95190b36fd236ad8afc1e7dc3f13ee234a
WintangZ <vinhttse172042@fpt.edu.vn>
2023-10-02 14:21:07 +0700

    Add arrow and animal swiper

--- src/pages/Animals/Components/AnimalWrapper.js
+++ src/pages/Animals/Components/AnimalWrapper.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, Mousewheel } from 'swiper/modules';
+import { Navigation, Pagination, Mousewheel, EffectCoverflow } from 'swiper/modules';
 import 'swiper/css'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import styles from './AnimalWrapper.module.scss'
+import './AnimalWrapper.css'
 import animals from '../AnimalsData';
+import arrow_right from '~/assets/img/arrow-right.png';
+import arrow_left from '~/assets/img/arrow-left.png';
 
 function AnimalWrapper() {
     const [id, setId] = useState(1);
     return (
         <Swiper className={`${styles.animal_wrapper}`}
-            modules={[Navigation, Pagination, Mousewheel]}
-            spaceBetween={5}
-            slidesPerView={5}
+            modules={[Navigation, Pagination, Mousewheel, EffectCoverflow]}
+            spaceBetween={0}
+            slidesPerView={4}
             navigation
-            pagination={{ clickable: true }}
             direction='horizontal'
-            loop='true'
-            mousewheel='true'>
+            loop='true'>
             {animals.map((animal) => (
                 <SwiperSlide key={animal.id} className={`${styles.card}`}>
                     <Link to={`/animals/${animal.id}`} onClick={() => setId(animal.id)} className={`${styles.animal}`}>
@@ -27,6 +28,12 @@ function AnimalWrapper() {
                     </Link>
                 </SwiperSlide>
             ))}
+            <div className={styles.arrow_right}>
+                <img src={arrow_right} />
+            </div>
+            <div className={styles.arrow_left}>
+                <img src={arrow_left} />
+            </div>
         </Swiper>
     )
 }
