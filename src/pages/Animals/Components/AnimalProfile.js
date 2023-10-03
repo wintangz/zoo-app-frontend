e2429e371db3bfd1de52d4bb2dd04ec8193646b4
WintangZ <vinhttse172042@fpt.edu.vn>
2023-10-03 14:37:39 +0700

    Done animal draft

--- src/pages/Animals/Components/AnimalProfile.js
+++ src/pages/Animals/Components/AnimalProfile.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styles from './AnimalProfile.module.scss'
+import animals from '../AnimalsData'
+
+import tiger from '~/assets/animals/tiger.jpg'
 
 function AnimalProfile() {
     return (
         <div className={styles.animal_profile}>
-            <div>
-                <div>Animal Name</div>
-                <div>This is an animal</div>
+            <div className={styles.animal_info}>
+                <div className={styles.title}>Tiger</div>
+                <div className={styles.content}>The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside. </div>
             </div>
 
-            <div>
-                <img />
+            <div className={styles.animal_picture}>
+                <img src={tiger} />
             </div>
         </div>
     )
