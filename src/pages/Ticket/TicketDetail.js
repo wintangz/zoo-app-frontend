71140b6e22ab7d1c9fe35f484e41c78a90d7749b
conboaduide <anhvnvse172006@fpt.edu.vn>
2023-10-04 15:00:53 +0700

    Done Ticket Label

---
+++ src/pages/Ticket/TicketDetail.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import styles from './Ticket.module.scss';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+
+export default function TicketDetail(props) {
+    const [value, setValue] = useState(0);
+    const PlusPrice = () => {
+        setValue(value + 1)
+        props.increaseQuantity(props.ticket.price)
+    }
+    const MinPrice = () => {
+        if (value > 0) { setValue(value - 1) }
+    }
+    return (
+        <tr className={styles.table_row}>
+            <td className={styles.table_data1}>
+                <img src={props.ticket.img} />
+            </td>
+            <td className={styles.table_data}>
+                <b>{props.ticket.name}</b>
+                <br />{props.ticket.info}
+            </td>
+            <td className={`${styles.table_data} ${styles.price}`}>{props.ticket.priceLabel}</td>
+            <td className={styles.table_data}>
+                <div className={styles.input}>
+                    <div className={styles.btn} onClick={MinPrice}><AiOutlineMinus /></div>
+                    <div className={styles.value}>{value}</div>
+                    <div className={styles.btn} onClick={PlusPrice}><AiOutlinePlus /></div>
+                </div>
+            </td>
+        </tr>
+    );
+}
