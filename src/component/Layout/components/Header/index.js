2948e5473d2aa8f315a39f8c7b4cd11e7bc7f80d
loctodalee <91656805+loctodalee@users.noreply.github.com>
2023-10-04 13:23:47 +0700

    full header

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,72 +1,3 @@
-// import classNames from 'classnames/bind';
-// import styles from './Header.module.scss';
-// import logo from '~/assets/img/logo-final.svg';
-// import { Link } from 'react-router-dom';
-
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
-
-// import { components } from './components';
-// import { Fragment } from 'react';
-// const cx = classNames.bind(styles);
-// function Header() {
-//     return (
-//         <header className={cx('wrapper')}>
-//             <div className={cx('header--logo')}>
-//                 <img alt="Logo" className={cx('header--logo--img')} src={logo} />
-//             </div>
-//             <div className={cx('nav')}>
-//                 <ul className={cx('nav--menu--main')}>
-//                     {components.map((component, index) => {
-//                         //Check if the component have sub navigation will create sub
-
-//                         if (component.sub) {
-//                             // Check sub is existing
-//                             return (
-//                                 <Fragment>
-//                                     <li key={index} className={cx('nav--menu--sub') + ' ' + cx('menu--sub')}>
-//                                         {component.name}
-//                                         <ul className={cx('sub--menu')}>
-//                                             {component.sub.map((object, index) => {
-//                                                 // Map to get all sub components
-//                                                 return (
-//                                                     <Fragment>
-//                                                         <li key={index} className={cx('sub--menu--object')}>
-//                                                             <Link to={object.path}>{object.name}</Link>
-//                                                         </li>
-//                                                     </Fragment>
-//                                                 );
-//                                             })}
-//                                         </ul>
-//                                     </li>
-//                                 </Fragment>
-//                             );
-//                         } else {
-//                             // no sub will be rendered
-//                             return (
-//                                 <li key={index} className={cx('nav--menu--sub')}>
-//                                     <Link to={component.path}>{component.name}</Link>
-//                                 </li>
-//                             );
-//                         }
-//                     })}
-//                 </ul>
-//                 <div href="" className={cx('nav--menu--cart') + ' ' + cx('nav--menu--sub')}>
-//                     <span className={cx('cart--icon')}>
-//                         <FontAwesomeIcon icon={faCartShopping} style={{ color: '#ffffff' }} />
-//                     </span>
-//                 </div>
-//                 <div className={cx('nav--menu--login') + ' ' + cx('nav--menu--sub')}>
-//                     <a className={cx('login-btn')} href="/#">
-//                         Login
-//                     </a>
-//                 </div>
-//             </div>
-//         </header>
-//     );
-// }
-
-// export default Header;
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import LoginForm from '../LoginForm/loginform';
@@ -79,10 +10,10 @@ import { NamePageContext } from '~/App';
 function Header() {
     const [open, setOpen] = useState(false);
     const NamePage = useContext(NamePageContext);
+    const lineRef = useRef(null);
     useEffect(() => {
         // Use document.querySelectorAll to select all elements with the class
         const headerNavItems = document.querySelectorAll(`.${styles.navitem_container}`);
-        console.log(headerNavItems)
         // Check if any elements were found
         if (headerNavItems.length > 0) {
             // Loop through the NodeList (similar to an HTMLCollection) using forEach
@@ -95,6 +26,9 @@ function Header() {
                 // Access the text content or perform other actions with the element
             });
         }
+
+
+
         return () => {
             headerNavItems.forEach((element) => {
                 element.classList.remove(styles.active);
@@ -102,6 +36,34 @@ function Header() {
         }
 
     }, [NamePage]);
+    useEffect(() => {
+        lineRef.current.style.left = "47.5%";
+    }, [])
+    const handleHover = (element) => {
+        // Move the .line element to the hovered element
+        // lineRef.current.style.left = `${element.offsetLeft}px`;
+        switch (element) {
+            case "Home": { lineRef.current.style.left = '47.5%'; lineRef.current.style.width = '38px'; break; }
+            case "News": { lineRef.current.style.left = '55.5%'; lineRef.current.style.width = '38px'; break; }
+            case "Explore": { lineRef.current.style.left = '62%'; lineRef.current.style.width = '80px'; break; }
+            case "Ticket": { lineRef.current.style.left = '71.2%'; lineRef.current.style.width = '42px'; break; }
+            case "About": { lineRef.current.style.left = '79%'; lineRef.current.style.width = '45px'; break; }
+        }
+    };
+    const handleUnHover = (element) => {
+        document.querySelector(`.${styles.active}`).classList.contains("Home");
+        if (document.querySelector(`.${styles.active}`).classList.contains("Home")) {
+            lineRef.current.style.left = '47.5%'; lineRef.current.style.width = '38px';
+        } else if (document.querySelector(`.${styles.active}`).classList.contains("News")) {
+            lineRef.current.style.left = '55.5%'; lineRef.current.style.width = '38px';
+        } else if (document.querySelector(`.${styles.active}`).classList.contains("Explore")) {
+            lineRef.current.style.left = '62%'; lineRef.current.style.width = '80px';
+        } else if (document.querySelector(`.${styles.active}`).classList.contains("Ticket")) {
+            lineRef.current.style.left = '71.2%'; lineRef.current.style.width = '42px';
+        } else if (document.querySelector(`.${styles.active}`).classList.contains("About")) {
+            lineRef.current.style.left = '79%'; lineRef.current.style.width = '45px';
+        }
+    }
     return (
         <>
             <header className={styles.container}>
@@ -110,53 +72,15 @@ function Header() {
                 </div>
                 <div className={styles.navwrap}>
                     <div className={styles.navbar}>
-                        {/* <div className={styles.navitem_container}>
-                            <Link to="/" className={styles.navitem}>
-                                Home
-                            </Link>
-                        </div>
-                        <div className={styles.navitem_container}>
-                            <Link to="/news" className={styles.navitem}>
-                                News
-                            </Link>
-                        </div>
-                        <div className={styles.navitem_container}>
-                            <div className={styles.navitem}>
-                                <span>Explore</span>
-                                <FontAwesomeIcon icon={faCaretDown} />
-                                <div className={styles.dropdown}>
-                                    <div className={styles.dropdown_item_container}>
-                                        <Link to="/animals/1" className={styles.dropdown_item}>
-                                            Animals
-                                        </Link>
-                                    </div>
-                                    <div className={styles.dropdown_item_container}>
-                                        <Link to="/habitats" className={styles.dropdown_item}>
-                                            Habitats
-                                        </Link>
-                                    </div>
-                                    <div className={styles.dropdown_item_container}>
-                                        <Link to="/maps" className={styles.dropdown_item}>
-                                            Zoo Map
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.navitem_container}>
-                            <Link to="/ticket" className={styles.navitem}>
-                                Ticket
-                            </Link>
-                        </div>
-                        <div className={styles.navitem_container}>
-                            <Link to="/about" className={styles.navitem}>
-                                About
-                            </Link>
-                        </div> */}
+                        <div className={styles.line} ref={lineRef} />
                         {components.map((component, index) => {
                             if (component.sub) {
                                 return (
-                                    <div key={index} name={component.name} className={styles.navitem_container}>
+                                    <div key={index}
+                                        className={`${styles.navitem_container} ${styles["item_" + index]}` + " " + component.name}
+                                        onMouseEnter={() => handleHover(component.name)}
+                                        onMouseLeave={() => handleUnHover(index)}
+                                    >
                                         <div key={index} className={styles.navitem}>
                                             <span>{component.name}</span>
                                             <FontAwesomeIcon icon={faCaretDown} />
@@ -176,7 +100,11 @@ function Header() {
                                 )
                             } else {
                                 return (
-                                    <div name={component.name} className={styles.navitem_container}>
+                                    <div name={component.name}
+                                        className={`${styles.navitem_container} ${styles["item_" + index]}` + " " + component.name}
+                                        onMouseEnter={() => handleHover(component.name)}
+                                        onMouseLeave={() => handleUnHover(index)}
+                                    >
                                         <Link to={component.path} className={styles.navitem}>
                                             {component.name}
                                         </Link>
