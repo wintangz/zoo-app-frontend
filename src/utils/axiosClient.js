fad2a2eb5da4a496e62b2940df99463645150a9c
WintangZ <vinhttse172042@fpt.edu.vn>
2023-10-09 01:19:36 +0700

    Fix header+footer font size

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://192.168.2.120:8080/api/'
-    baseURL: 'hhtp://localhost:8080/api/'
+    baseURL: 'http://localhost:8080/api/'
 })
 export const get = async (path, options = {}) => {
     const response = await api.get(path, options = {});
