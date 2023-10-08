113d46743daa7137037a44749bfb54db02efc0ca
Hoàng Thành <hoangthanh5264@gmail.com>
2023-10-08 16:23:14 +0700

    update register

--- src/component/Layout/components/RegisterForm/index.js
+++ src/component/Layout/components/RegisterForm/index.js
@@ -1,11 +1,73 @@
-<<<<<<< HEAD
-import styles from './RegisterForm.module.scss'
 import React, { useEffect } from 'react';
-=======
-import styles from './RegisterForm.module.scss';
->>>>>>> 0468230353a6bd31205a64f6ae8f010d86accdde
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import jwt_decode from "jwt-decode";
+import * as Yup from 'yup';
+import axios from 'axios';
+
+import styles from './RegisterForm.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+
+const RegisterSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Confirm Password is required'),
+    firstName: Yup.string().required('First Name is required'),
+    lastName: Yup.string().required('Last Name is required'),
+    birthDate: Yup.date().required('Birth Date is required'),
+    citizenID: Yup.string().required('Citizen ID is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    phoneNumber: Yup.string().required('Phone Number is required'),
+    address: Yup.string().required('Address is required'),
+    country: Yup.string().required('Country is required'),
+});
 
 function RegisterForm({ setOpenLoginForm, setOpenRegisterForm }) {
+
+    const initialValues = {
+        username: '',
+        password: '',
+        confirmPassword: '',
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        citizenID: '',
+        email: '',
+        phoneNumber: '',
+        address: '',
+        country: '',
+    }
+
+    const handleRegister = async (values, { setSubmitting }) => {
+        try {
+            // Send a POST request to the registration API
+            const response = await axios.post('http://localhost:8080/api/auth/register', values);
+
+            // Handle the response as needed
+            localStorage.setItem('token', response.data.accessToken);
+            var token = response.data.accessToken;
+            var decode = jwt_decode(token);
+
+            if (response.status === 200) {
+                // Perform actions based on the registration success
+                decode.roles.map((role) => {
+                    if (role === 'ADMIN') {
+                        window.location = '/mainPage';
+                    }
+                });
+            }
+
+            // Close the modal or perform other actions
+            setOpenRegisterForm(false);
+        } catch (error) {
+            // Handle errors
+            console.error('Error during registration:', error);
+        } finally {
+            setSubmitting(false);
+        }
+    };
 
     const countries = [
         "VietNam",
@@ -61,86 +123,116 @@ function RegisterForm({ setOpenLoginForm, setOpenRegisterForm }) {
         <>
             <div className={styles.overlay}>
                 <div className={styles.container}>
-                    <h1>Register</h1>
-                    <form action='#' className={styles.form}>
-                        <div className={styles.inputBox}>
-                            <label>Username</label>
-                            <input type='text' placeholder="Username" />
-                        </div>
-                        <div className={styles.column}>
-                            <div className={styles.inputBox}>
-                                <label>Password</label>
-                                <input type='password' placeholder="Password" />
-                            </div>
-                            <div className={styles.inputBox}>
-                                <label>Confirm Password</label>
-                                <input type='password' placeholder="Confirm Password" />
-                            </div>
-                        </div>
-                        <div className={styles.column}>
-                            <div className={styles.inputBox}>
-                                <label>First Name</label>
-                                <input type='text' placeholder="First Name" />
-                            </div>
-                            <div className={styles.inputBox}>
-                                <label>Last Name</label>
-                                <input type='text' placeholder="Last Name" />
-                            </div>
-                            <div className={styles.inputBox}>
-                                <label>Phone Number</label>
-                                <input type='text' placeholder="Phone Number" />
-                            </div>
-                        </div>
-                        <div className={styles.column}>
-                            <div className={styles.inputBox}>
-                                <label>Citizen ID</label>
-                                <input type='text' placeholder="Citizen ID" />
-                            </div>
-                            <div className={styles.selectBox}>
-                                <label>Country</label>
-                                <select>
-                                    <option hidden>Country</option>
-                                    {countries.map((country, index) => (
-                                        <option key={index}>{country}</option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className={styles.inputBox}>
-                                <label>Birth Date</label>
-                                <input type='date' />
-                            </div>
-                        </div>
-                        <div className={styles.genderCheck}>
-                            <h3>Gender</h3>
-                            <div className={styles.gender}>
-                                <div className={styles.option}>
-                                    <input type='radio' id='check-male' name='gender' checked />
-                                    <label for='check-male'>Male</label>
-                                </div>
-                                <div className={styles.option}>
-                                    <input type='radio' id='check-female' name='gender' />
-                                    <label for='check-female'>Female</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.inputBox}>
-                            <label>Email</label>
-                            <input type='text' placeholder="Email" />
-                        </div>
-                        <div className={styles.inputBox}>
-                            <label>Address</label>
-                            <input type='text' placeholder="Address" />
-                        </div>
-                    </form>
-
-                    <div className={styles.linkBtn_warp}>
-                        Have an Account, <b onClick={handleLoginClick} className={styles.linkBtn}>Login now!!!</b>
+                    <div className={styles.close} onClick={() => setOpenRegisterForm(false)}>
+                        <FontAwesomeIcon icon={faClose} />
                     </div>
+                    <h1>Register</h1>
+
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={handleRegister}
+                        validationSchema={RegisterSchema}
+                    >
+                        <Form action='#' className={styles.form}>
+                            <div className={styles.inputBox}>
+                                <label>Username</label>
+                                <Field type='text' name='username' placeholder='Username' />
+                                <ErrorMessage
+                                    name='username'
+                                    component='div'
+                                    className={styles.error}
+                                />
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles.inputBox}>
+                                    <label>Password</label>
+                                    <Field type='password' name='password' placeholder="Password" />
+                                    <ErrorMessage name='password' component='div' className={styles.error} />
+                                </div>
+                                <div className={styles.inputBox}>
+                                    <label>Confirm Password</label>
+                                    <Field type='password' name='confirmPassword' placeholder="Confirm Password" />
+                                    <ErrorMessage name='confirmPassword' component='div' className={styles.error} />
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles.inputBox}>
+                                    <label>First Name</label>
+                                    <Field type='text' name='firstName' placeholder="First Name" />
+                                    <ErrorMessage name='firstName' component='div' className={styles.error} />
+                                </div>
+                                <div className={styles.inputBox}>
+                                    <label>Last Name</label>
+                                    <Field type='text' name='lastName' placeholder="Last Name" />
+                                    <ErrorMessage name='lastName' component='div' className={styles.error} />
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles.inputBox}>
+                                    <label>Birth Date</label>
+                                    <Field type='date' name='birthDate' />
+                                    <ErrorMessage name='birthDate' component='div' className={styles.error} />
+                                </div>
+                                <div className={styles.inputBox}>
+                                    <label>Citizen ID</label>
+                                    <Field type='text' name='citizenID' placeholder="Citizen ID" />
+                                    <ErrorMessage name='citizenID' component='div' className={styles.error} />
+                                </div>
+                            </div>
+                            <div className={styles.genderCheck}>
+                                <h3>Gender</h3>
+                                <div className={styles.gender}>
+                                    <div className={styles.option}>
+                                        <Field type='radio' id='check-male' name='gender' value='male' />
+                                        <label htmlFor='check-male'>Male</label>
+                                    </div>
+                                    <div className={styles.option}>
+                                        <Field type='radio' id='check-female' name='gender' value='female' />
+                                        <label htmlFor='check-female'>Female</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles.inputBox}>
+                                    <label>Email</label>
+                                    <Field type='text' name='email' placeholder="Email" />
+                                    <ErrorMessage name='email' component='div' className={styles.error} />
+                                </div>
+                                <div className={styles.inputBox}>
+                                    <label>Phone Number</label>
+                                    <Field type='text' name='phoneNumber' placeholder="Phone Number" />
+                                    <ErrorMessage name='phoneNumber' component='div' className={styles.error} />
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles.inputBox}>
+                                    <label>Address</label>
+                                    <Field type='text' name='address' placeholder="Address" />
+                                    <ErrorMessage name='address' component='div' className={styles.error} />
+                                </div>
+                                <div className={styles.selectBox}>
+                                    <label>Country</label>
+                                    <Field as='select' name='country'>
+                                        <option hidden>Country</option>
+                                        {countries.map((country, index) => (
+                                            <option key={index} value={country}>
+                                                {country}
+                                            </option>
+                                        ))}
+                                    </Field>
+                                    <ErrorMessage name='country' component='div' className={styles.error} />
+                                </div>
+                            </div>
+                            <div className={styles.linkBtn_warp}>
+                                Have an Account, <b onClick={handleLoginClick} className={styles.linkBtn}>Login now!!!</b>
+                            </div>
 
 
-                    <button className={styles.submit}>
-                        Register
-                    </button>
+                            <button className={styles.submit} type='submit' id='register'>
+                                Register
+                            </button>
+                        </Form>
+                    </Formik>
                 </div>
             </div>
         </>
