9c85d0b04f31962b6deb0049d95e5578c59e0499
Hoàng Thành <hoangthanh5264@gmail.com>
2023-10-11 01:05:28 +0700

    update gallery

--- src/pages/Habitats/Gallery/Gallery.js
+++ src/pages/Habitats/Gallery/Gallery.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Gallery.module.scss';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Gallery({ item }) {
 
+
+    const [modal, setModal] = useState(false);
+    const [tempimgUrl, setTempimgUrl] = useState('');
+    const getImg = (imgUrl) => {
+        setTempimgUrl(imgUrl);
+        setModal(true);
+        console.warn(imgUrl);
+    }
+
     return (
         <>
+            <div
+                className={`${styles.modal} ${modal ? styles.open : ''}`}>
+                <img src={tempimgUrl} />
+                <CloseIcon onClick={() => setModal(false)} />
+            </div>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    {item.map((val) => (
-                        <div key={val.id} className={styles.galleryItem}>
-                            <div className={styles.image} /*key={index} onClick={() => getImg(item.linkImg)}*/>
-                                <img src={val.imgUrl} style={{ width: '100%' }} loading='lazy' />
-                                <div className={styles.name}>
-                                    {val.name}
+                    {item.map((val, index) => {
+                        return (
+                            <div className={styles.galleryItem} key={index} onClick={() => getImg(val.imgUrl)}>
+                                <div className={styles.image} >
+                                    <img src={val.imgUrl} style={{ width: '100%' }} loading='lazy' />
+                                    <div className={styles.name}>
+                                        {val.name}
+                                    </div>
                                 </div>
-                            </div>
 
-                        </div>
-                    ))}
+                            </div>
+                        )
+
+                    })}
                 </div>
             </div>
         </>
--- src/pages/Habitats/HabitatZone/HabitatZone.js
+++ src/pages/Habitats/HabitatZone/HabitatZone.js
@@ -1,48 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./HabitatZone.module.scss";
 import Gallery from "../Gallery/Gallery";
-import InfiniteScroll from "react-infinite-scroll-hook";
-
 import classNames from "classnames/bind";
+
+
 const cx = classNames.bind(styles);
 
 function HabitatZone({ animals, habitats }) {
-    const [items, setItems] = useState(animals || []);
-    const habitatZone = [...new Set((habitats || []).map((val) => val.name))];
-    const [selectedZone, setSelectedZone] = useState(null);
 
+
+    const [item, setItem] = useState(animals || []);
+    const habitatZone = [...new Set((habitats || []).map((val) => val.name))];
     const filterHabitat = (temp) => {
         const newHabitat = animals.filter((newval) => newval.habitat === temp);
-        setItems(newHabitat);
-    };
+        setItem(newHabitat);
+    }
 
+    const [selectedZone, setSelectedZone] = useState(null);
     const handleZoneClick = (zone) => {
         filterHabitat(zone);
-        setSelectedZone(selectedZone === zone ? null : zone);
+        setSelectedZone(selectedZone === zone ? null : zone); // Toggle the selected zone
     };
-
-    const loadMoreItems = () => {
-        // Add logic to load more items (e.g., fetch from API)
-        // For this example, I'm just adding 5 more items each time
-        const moreItems = (animals || []).slice(items.length, items.length + 5);
-        setItems([...items, ...moreItems]);
-    };
-
-    const [infiniteRef] = InfiniteScroll({
-        loading: false, // You can set this to true if you want to load items initially
-        hasNextPage: true, // Set to false when you've loaded all items
-        onLoadMore: loadMoreItems,
-        rootMargin: "0px 0px 100px 0px", // Adjust as needed
-    });
 
     return (
         <div className={styles.btnList}>
             {habitatZone.map((zone) => (
-                <div key={zone} className={cx("zone")} onClick={() => handleZoneClick(zone)}>
+                <div key={zone} className={cx("zone")} >
                     {habitats
                         .filter((component) => component.name === zone)
                         .map((component) => (
-                            <div key={component.name} className={cx("zone--container")}>
+                            <div key={component.name} className={cx("zone--container")} onClick={() => handleZoneClick(zone)}>
                                 <div className={cx("overlay")}></div>
                                 <div
                                     className={cx("zone--background")}
@@ -55,14 +42,25 @@ function HabitatZone({ animals, habitats }) {
                                 ></div>
                                 <div className={cx("zone--title")}>{component.name}</div>
                                 <div className={cx("zone--title-hover")}></div>
+                                {/* <div
+                                    className={cx("zone--hover")}
+                                    style={{
+                                        backgroundImage: `url(${hoverImg})`,
+                                        backgroundRepeat: "no-repeat",
+                                        backgroundSize: "30%",
+                                        right: "-30%",
+                                        width: "50%",
+                                        height: "100%",
+                                        position: "absolute",
+                                        opacity: 0,
+                                    }}
+                                ></div> */}
                             </div>
                         ))}
                     {/* Add Gallery component for each zone */}
                     {selectedZone === zone && (
                         <div key={`gallery-${zone}`} className={styles.galleryWrapper}>
-                            <div ref={infiniteRef}>
-                                <Gallery item={items.filter((item) => item.habitat === zone)} />
-                            </div>
+                            <Gallery item={animals.filter((item) => item.habitat === zone)} />
                         </div>
                     )}
                 </div>
@@ -70,5 +68,4 @@ function HabitatZone({ animals, habitats }) {
         </div>
     );
 }
-
 export default HabitatZone;
